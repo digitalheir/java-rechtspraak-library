cf109ae33502f0f376dd47609f42e4a755b7ed7a
Maarten <m.f.a.trompper@uva.nl>
2016-05-12 00:34:29 +0200

    Some extra words

---
+++ src/components/Figures/figure-word-count-title/data-full.js
@@ -0,0 +1,197 @@
+export default {
+    "url": "https://rechtspraak.cloudant.com/docs/_design/stats/_view/word_count_for_title_elements?group_level=1&stale=ok",
+    "data": [{"wordCount": 1, "titleCount": 74646}, {"wordCount": 2, "titleCount": 51409}, {
+        "wordCount": 3,
+        "titleCount": 67914
+    }, {"wordCount": 4, "titleCount": 23850}, {"wordCount": 5, "titleCount": 28579}, {
+        "wordCount": 6,
+        "titleCount": 33224
+    }, {"wordCount": 7, "titleCount": 11728}, {"wordCount": 8, "titleCount": 5950}, {
+        "wordCount": 9,
+        "titleCount": 4947
+    }, {"wordCount": 10, "titleCount": 4661}, {"wordCount": 11, "titleCount": 1580}, {
+        "wordCount": 12,
+        "titleCount": 986
+    }, {"wordCount": 13, "titleCount": 444}, {"wordCount": 14, "titleCount": 207}, {
+        "wordCount": 15,
+        "titleCount": 91
+    }, {"wordCount": 16, "titleCount": 37}, {"wordCount": 17, "titleCount": 82}, {
+        "wordCount": 18,
+        "titleCount": 41
+    }, {"wordCount": 19, "titleCount": 40}, {"wordCount": 20, "titleCount": 29}, {
+        "wordCount": 21,
+        "titleCount": 20
+    }, {"wordCount": 22, "titleCount": 26}, {"wordCount": 23, "titleCount": 23}, {
+        "wordCount": 24,
+        "titleCount": 26
+    }, {"wordCount": 25, "titleCount": 13}, {"wordCount": 26, "titleCount": 22}, {
+        "wordCount": 27,
+        "titleCount": 30
+    }, {"wordCount": 28, "titleCount": 25}, {"wordCount": 29, "titleCount": 30}, {
+        "wordCount": 30,
+        "titleCount": 16
+    }, {"wordCount": 31, "titleCount": 18}, {"wordCount": 32, "titleCount": 13}, {
+        "wordCount": 33,
+        "titleCount": 22
+    }, {"wordCount": 34, "titleCount": 11}, {"wordCount": 35, "titleCount": 16}, {
+        "wordCount": 36,
+        "titleCount": 13
+    }, {"wordCount": 37, "titleCount": 7}, {"wordCount": 38, "titleCount": 13}, {
+        "wordCount": 39,
+        "titleCount": 14
+    }, {"wordCount": 40, "titleCount": 18}, {"wordCount": 41, "titleCount": 10}, {
+        "wordCount": 42,
+        "titleCount": 9
+    }, {"wordCount": 43, "titleCount": 12}, {"wordCount": 44, "titleCount": 16}, {
+        "wordCount": 45,
+        "titleCount": 14
+    }, {"wordCount": 46, "titleCount": 9}, {"wordCount": 47, "titleCount": 11}, {
+        "wordCount": 48,
+        "titleCount": 9
+    }, {"wordCount": 49, "titleCount": 13}, {"wordCount": 50, "titleCount": 4}, {
+        "wordCount": 51,
+        "titleCount": 8
+    }, {"wordCount": 52, "titleCount": 7}, {"wordCount": 53, "titleCount": 11}, {
+        "wordCount": 54,
+        "titleCount": 3
+    }, {"wordCount": 55, "titleCount": 9}, {"wordCount": 56, "titleCount": 7}, {
+        "wordCount": 57,
+        "titleCount": 10
+    }, {"wordCount": 58, "titleCount": 12}, {"wordCount": 59, "titleCount": 13}, {
+        "wordCount": 60,
+        "titleCount": 12
+    }, {"wordCount": 61, "titleCount": 4}, {"wordCount": 62, "titleCount": 6}, {
+        "wordCount": 63,
+        "titleCount": 10
+    }, {"wordCount": 64, "titleCount": 4}, {"wordCount": 65, "titleCount": 7}, {
+        "wordCount": 66,
+        "titleCount": 11
+    }, {"wordCount": 67, "titleCount": 6}, {"wordCount": 68, "titleCount": 3}, {
+        "wordCount": 69,
+        "titleCount": 6
+    }, {"wordCount": 70, "titleCount": 10}, {"wordCount": 72, "titleCount": 4}, {
+        "wordCount": 73,
+        "titleCount": 4
+    }, {"wordCount": 74, "titleCount": 13}, {"wordCount": 75, "titleCount": 5}, {
+        "wordCount": 76,
+        "titleCount": 5
+    }, {"wordCount": 77, "titleCount": 10}, {"wordCount": 78, "titleCount": 5}, {
+        "wordCount": 79,
+        "titleCount": 6
+    }, {"wordCount": 80, "titleCount": 3}, {"wordCount": 81, "titleCount": 4}, {
+        "wordCount": 82,
+        "titleCount": 3
+    }, {"wordCount": 83, "titleCount": 2}, {"wordCount": 84, "titleCount": 5}, {
+        "wordCount": 85,
+        "titleCount": 1
+    }, {"wordCount": 87, "titleCount": 4}, {"wordCount": 88, "titleCount": 3}, {
+        "wordCount": 89,
+        "titleCount": 2
+    }, {"wordCount": 90, "titleCount": 5}, {"wordCount": 91, "titleCount": 4}, {
+        "wordCount": 93,
+        "titleCount": 3
+    }, {"wordCount": 94, "titleCount": 5}, {"wordCount": 95, "titleCount": 3}, {
+        "wordCount": 96,
+        "titleCount": 5
+    }, {"wordCount": 97, "titleCount": 2}, {"wordCount": 98, "titleCount": 1}, {
+        "wordCount": 99,
+        "titleCount": 2
+    }, {"wordCount": 101, "titleCount": 1}, {"wordCount": 102, "titleCount": 2}, {
+        "wordCount": 103,
+        "titleCount": 3
+    }, {"wordCount": 104, "titleCount": 4}, {"wordCount": 105, "titleCount": 6}, {
+        "wordCount": 106,
+        "titleCount": 4
+    }, {"wordCount": 107, "titleCount": 1}, {"wordCount": 108, "titleCount": 3}, {
+        "wordCount": 109,
+        "titleCount": 1
+    }, {"wordCount": 110, "titleCount": 2}, {"wordCount": 111, "titleCount": 4}, {
+        "wordCount": 112,
+        "titleCount": 4
+    }, {"wordCount": 113, "titleCount": 2}, {"wordCount": 114, "titleCount": 4}, {
+        "wordCount": 115,
+        "titleCount": 1
+    }, {"wordCount": 116, "titleCount": 1}, {"wordCount": 117, "titleCount": 2}, {
+        "wordCount": 118,
+        "titleCount": 3
+    }, {"wordCount": 119, "titleCount": 2}, {"wordCount": 120, "titleCount": 3}, {
+        "wordCount": 121,
+        "titleCount": 1
+    }, {"wordCount": 122, "titleCount": 3}, {"wordCount": 124, "titleCount": 2}, {
+        "wordCount": 125,
+        "titleCount": 1
+    }, {"wordCount": 127, "titleCount": 1}, {"wordCount": 128, "titleCount": 4}, {
+        "wordCount": 129,
+        "titleCount": 2
+    }, {"wordCount": 130, "titleCount": 2}, {"wordCount": 131, "titleCount": 3}, {
+        "wordCount": 133,
+        "titleCount": 4
+    }, {"wordCount": 134, "titleCount": 1}, {"wordCount": 135, "titleCount": 6}, {
+        "wordCount": 136,
+        "titleCount": 1
+    }, {"wordCount": 137, "titleCount": 2}, {"wordCount": 138, "titleCount": 3}, {
+        "wordCount": 139,
+        "titleCount": 2
+    }, {"wordCount": 142, "titleCount": 1}, {"wordCount": 143, "titleCount": 1}, {
+        "wordCount": 144,
+        "titleCount": 4
+    }, {"wordCount": 145, "titleCount": 2}, {"wordCount": 146, "titleCount": 2}, {
+        "wordCount": 147,
+        "titleCount": 1
+    }, {"wordCount": 148, "titleCount": 3}, {"wordCount": 151, "titleCount": 9}, {
+        "wordCount": 153,
+        "titleCount": 1
+    }, {"wordCount": 156, "titleCount": 1}, {"wordCount": 157, "titleCount": 1}, {
+        "wordCount": 159,
+        "titleCount": 1
+    }, {"wordCount": 162, "titleCount": 2}, {"wordCount": 163, "titleCount": 1}, {
+        "wordCount": 164,
+        "titleCount": 1
+    }, {"wordCount": 166, "titleCount": 1}, {"wordCount": 168, "titleCount": 1}, {
+        "wordCount": 173,
+        "titleCount": 1
+    }, {"wordCount": 174, "titleCount": 2}, {"wordCount": 175, "titleCount": 1}, {
+        "wordCount": 177,
+        "titleCount": 1
+    }, {"wordCount": 182, "titleCount": 1}, {"wordCount": 183, "titleCount": 1}, {
+        "wordCount": 189,
+        "titleCount": 2
+    }, {"wordCount": 190, "titleCount": 1}, {"wordCount": 193, "titleCount": 1}, {
+        "wordCount": 195,
+        "titleCount": 1
+    }, {"wordCount": 197, "titleCount": 1}, {"wordCount": 199, "titleCount": 1}, {
+        "wordCount": 200,
+        "titleCount": 1
+    }, {"wordCount": 201, "titleCount": 1}, {"wordCount": 202, "titleCount": 1}, {
+        "wordCount": 203,
+        "titleCount": 2
+    }, {"wordCount": 208, "titleCount": 1}, {"wordCount": 224, "titleCount": 1}, {
+        "wordCount": 227,
+        "titleCount": 1
+    }, {"wordCount": 228, "titleCount": 1}, {"wordCount": 229, "titleCount": 1}, {
+        "wordCount": 236,
+        "titleCount": 1
+    }, {"wordCount": 237, "titleCount": 1}, {"wordCount": 251, "titleCount": 1}, {
+        "wordCount": 257,
+        "titleCount": 1
+    }, {"wordCount": 260, "titleCount": 1}, {"wordCount": 266, "titleCount": 1}, {
+        "wordCount": 267,
+        "titleCount": 1
+    }, {"wordCount": 273, "titleCount": 1}, {"wordCount": 276, "titleCount": 1}, {
+        "wordCount": 303,
+        "titleCount": 1
+    }, {"wordCount": 306, "titleCount": 2}, {"wordCount": 307, "titleCount": 1}, {
+        "wordCount": 337,
+        "titleCount": 1
+    }, {"wordCount": 373, "titleCount": 1}, {"wordCount": 427, "titleCount": 1}, {
+        "wordCount": 456,
+        "titleCount": 1
+    }, {"wordCount": 487, "titleCount": 1}, {"wordCount": 496, "titleCount": 1}, {
+        "wordCount": 605,
+        "titleCount": 1
+    }, {"wordCount": 642, "titleCount": 1}, {"wordCount": 667, "titleCount": 1}, {
+        "wordCount": 731,
+        "titleCount": 1
+    }, {"wordCount": 1036, "titleCount": 1}],
+    "totalCount": 311368
+}
